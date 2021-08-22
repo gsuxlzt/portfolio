5f328ad3df2f220322cc8d4421f7cc2404d4c086
Jami Carangan <jami.carangan@gmail.com>
2021-08-22 11:35:12 +0800

    :sparkles: add ApolloProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider, CSSReset, theme } from '@chakra-ui/react'
+import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+import { ChakraProvider, CSSReset, theme } from "@chakra-ui/react";
+import client from "../lib/apolloClient";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
         <CSSReset />
         <Component {...pageProps} />
-    </ChakraProvider>
-  )
+      </ChakraProvider>
+    </ApolloProvider>
+  );
 }
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,48 @@
-import { Heading, Link, Container, Stack, Text } from '@chakra-ui/react'
+import React from "react";
+import { gql } from "@apollo/client";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { Heading, Link, Container, Stack, Text } from "@chakra-ui/react";
+import client from "../lib/apolloClient";
 
-export default function Home() {
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query GetProfile($id: String!) {
+        profile(id: $id) {
+          title
+          position
+          shortDescription {
+            json
+          }
+          socialLinks
+        }
+      }
+    `,
+    variables: {
+      id: process.env.CONTENTFUL_PROFILE_ID,
+    },
+  });
+
+  console.log("data", data);
+  return {
+    props: {},
+  };
+};
+
+const IndexPage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-      <Container centerContent padding={['4','3','2']}>
-        <Heading as="h1" size="2xl" mb="2">
-          Welcome to <Link href="https://nextjs.org" color="purple.500">Next.js!</Link>
-        </Heading>
-        <Stack direction="row" w="full" background="purple.50">
-            <Text>test</Text>
-        </Stack>
+    <Container centerContent padding={["4", "3", "2"]}>
+      <Heading as="h1" size="2xl" mb="2">
+        Welcome to{" "}
+        <Link href="https://nextjs.org" color="purple.500">
+          Next.js!
+        </Link>
+      </Heading>
+      <Stack direction="row" w="full" background="purple.50">
+        <Text>test</Text>
+      </Stack>
     </Container>
-  )
-}
+  );
+};
+
+export default IndexPage;
