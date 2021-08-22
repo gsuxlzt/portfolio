30ac15fe38227c3e9a0beb2cbfba03aeba273e32
Jami Carangan <jami.carangan@gmail.com>
2021-08-22 15:59:08 +0800

    :sparkles: add home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,26 @@
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
-import { ChakraProvider, CSSReset, theme } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset, extendTheme, Box } from "@chakra-ui/react";
+import "@fontsource/raleway/700.css";
+import "@fontsource/inter/400.css";
+
 import client from "../lib/apolloClient";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Raleway, sans-serif",
+    body: "Inter, sans-serif",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <ChakraProvider theme={theme}>
         <CSSReset />
-        <Component {...pageProps} />
+        <Box bg="whitesmoke" minH="100vh">
+          <Component {...pageProps} />
+        </Box>
       </ChakraProvider>
     </ApolloProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import { gql } from "@apollo/client";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
-import { Heading, Link, Container, Stack, Text } from "@chakra-ui/react";
+import Head from "next/head";
+import {
+  Image,
+  Heading,
+  Link,
+  Container,
+  Stack,
+  Text,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { IconType } from "react-icons";
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 import client from "../lib/apolloClient";
+
+interface ISocialLinks {
+  href: string;
+  icon: string;
+  title: string;
+}
+
+const ICONS: { [key: string]: IconType } = {
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await client.query({
@@ -15,6 +40,10 @@ export const getStaticProps: GetStaticProps = async () => {
             json
           }
           socialLinks
+          picture {
+            url
+            title
+          }
         }
       }
     `,
@@ -23,25 +52,93 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   });
 
-  console.log("data", data);
+  if (!data.profile) {
+    return {
+      notFound: true,
+    };
+  }
   return {
-    props: {},
+    props: { ...data.profile },
+    revalidate: 10,
   };
 };
 
 const IndexPage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const {
+    title,
+    position,
+    shortDescription: { json: description },
+    socialLinks: profileSocialLinks,
+    picture: { url: imageUrl, title: imageTitle },
+  } = props;
+
+  const socialLinks: ISocialLinks[] = Object.values(profileSocialLinks);
+  const renderedDescription = documentToReactComponents(description);
+
   return (
-    <Container centerContent padding={["4", "3", "2"]}>
-      <Heading as="h1" size="2xl" mb="2">
-        Welcome to{" "}
-        <Link href="https://nextjs.org" color="purple.500">
-          Next.js!
-        </Link>
-      </Heading>
-      <Stack direction="row" w="full" background="purple.50">
-        <Text>test</Text>
-      </Stack>
-    </Container>
+    <>
+      <Head>
+        <title>Jan Miguel Carangan - Web Developer Portfolio</title>
+      </Head>
+      <Container
+        centerContent
+        maxW="container.lg"
+        padding={[6, 6, 4]}
+        minH="100vh"
+        sx={{
+          justifyContent: "center",
+        }}
+      >
+        <Stack direction={["column", "column", "row"]} w="full" spacing={6}>
+          <Stack
+            direction="column"
+            spacing={1}
+            sx={{
+              flexShrink: "0",
+              alignItems: ["center", "center", "flex-start"],
+            }}
+          >
+            <Image
+              align="center"
+              boxSize="150px"
+              borderRadius="full"
+              src={imageUrl}
+              alt={imageTitle}
+              sx={{
+                alignSelf: "center",
+              }}
+            />
+            <Heading as="h1" size="xl" pt="6" color="black">
+              {title}
+            </Heading>
+            <Text as="h4" fontSize="2xl" color="black">
+              {position}
+            </Text>
+            <Stack direction="row" mt={3} spacing={3}>
+              {socialLinks.map(({ href, title, icon }) => (
+                <Link href={href} key={title}>
+                  <Icon as={ICONS[icon]} w={8} h={8} color="black" />
+                </Link>
+              ))}
+            </Stack>
+          </Stack>
+          <Stack pt={5} color="blackAlpha.800">
+            <Heading as="h2" size="lg" mb={4} color="black">
+              Biography
+            </Heading>
+            {renderedDescription}
+            <Stack direction="column" spacing={2} pt={4}>
+              <Link sx={{ textDecoration: "underline" }}>
+                Check out my work history
+              </Link>
+              <Link sx={{ textDecoration: "underline" }}>
+                Read some articles on my dev journal
+              </Link>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
   );
 };
 
